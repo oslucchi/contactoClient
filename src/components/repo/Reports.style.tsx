3f5f26b1dd98f25a89294745262a57ea86431b98
Stefanama <amarie.stefan@gmail.com>
2023-10-27 17:44:26 +0200

    add speech to text

--- src/components/repo/Reports.style.tsx
+++ src/components/repo/Reports.style.tsx
@@ -1,42 +1,42 @@
-import { StyleSheet } from "react-native";
-import { Dimensions } from "react-native";
+import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
 
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   mainContainer: {
     paddingTop: 5,
     height: height - 5,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF',
   },
   header: {
-    height: "8%",
-    width: "95%",
-    alignSelf: "center",
-    backgroundColor: "white",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    directions: "inherit",
-    flexWrap: "nowrap",
+    height: '8%',
+    width: '95%',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    directions: 'inherit',
+    flexWrap: 'nowrap',
   },
   iconContainer: {
     width: 40,
     height: 40,
   },
   reportsContainer: {
-    maxHeight: "45%",
-    width: "95%",
-    alignSelf: "center",
-    backgroundColor: "#DCF8C6",
+    maxHeight: '45%',
+    width: '95%',
+    alignSelf: 'center',
+    backgroundColor: '#DCF8C6',
   },
   footer: {
     flex: 1,
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
   },
   reportItem: {
-    width: "95%",
+    width: '95%',
   },
 });
 
