9da4d4bb12a3c715b52832c80b87e26cca612e82
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2025-05-22 18:09:58 +0200

    trying to build

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import 'react-native-gesture-handler';
-import React from 'react';
 import { enableScreens } from 'react-native-screens';
 
 import {createStackNavigator} from '@react-navigation/stack';
